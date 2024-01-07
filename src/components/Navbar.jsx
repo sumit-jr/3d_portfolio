5f0a78fb96e1e9f47dd649560a82c26d1c888926
sumit-jr <er.sahsumit@gmail.com>
2024-01-07 17:12:24 +0530

    added assets

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,23 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
+
+import { logo } from "../assets/images";
 
 const Navbar = () => {
-  return (
-    <header className="header">
-        <NavLink to='/' className='w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md'> 
-        <p className="blue-gradient_text">SS</p>
-        </NavLink>
+    return (
+        <header className='header'>
+            <NavLink to='/' className='w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md'>
+                <p className="blue-gradient_text">SS</p>
+            </NavLink>
+            <nav className='flex text-lg gap-7 font-medium'>
+                <NavLink to='/about' className={({ isActive }) => isActive ? "text-blue-600" : "text-black"}>
+                    About
+                </NavLink>
+                <NavLink to='/projects' className={({ isActive }) => isActive ? "text-blue-600" : "text-black"}>
+                    Projects
+                </NavLink>
+            </nav>
+        </header>
+    );
+};
 
-        <nav className="flex text-lg gap-7 font-medium">
-            <NavLink to='/about' className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>
-                About
-            </NavLink>
-            <NavLink to='/projects' className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>
-                Projects
-            </NavLink>
-            <NavLink to='/contact' className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>
-                Contact
-            </NavLink>
-        </nav>
-    </header>
-  )
-}
-
-export default Navbar
+export default Navbar;
---
+++ src/constants/index.js
@@ -0,0 +1,145 @@
+import { starbucks, oasis } from "../assets/images";
+import {
+    contact,
+    css,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    summiz,
+    tailwindcss,
+    tensorflow,
+    typescript,
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: motion,
+        name: "Motion",
+        type: "Animation",
+    },
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tensorflow,
+        name: "tensorflow",
+        type: "ML",
+    }
+];
+
+export const experiences = [
+    {
+        title: "Data Scientist",
+        company_name: "Oasis Infobyte",
+        icon: oasis,
+        iconBg: "#accbe1",
+        date: "July 2023 - August 2023",
+        points: [
+            "Data Preprocessing: Enhancing data quality for reliable analysis.",
+            "Exploratory Data Analysis (EDA): Uncovering insights and trends in data.",
+            "Model Evaluation: Assessing and fine-tuning model accuracy and efficiency.",
+            "Performance Comparison: Benchmarking and contrasting model outcomes for optimal selection.",
+        ],
+    }
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/sumit-jr',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/sumit-prasad-sah/',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-red',
+        name: 'Amazon Price Tracker',
+        description: 'Developed a web application that tracks and notifies users of price changes for products on Amazon, helping users find the best deals.',
+        link: 'https://github.com/sumit-jr/pricewise',
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-yellow',
+        name: 'AI Summarizer Application',
+        description: 'App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.',
+        link: 'https://github.com/adrianhajdin/project_ai_summarizer',
+    }
+];
